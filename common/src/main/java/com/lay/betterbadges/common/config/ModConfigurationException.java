3d425992bd7257f985cfe76af2d997c2e7efee0a
MistahLay <clarencetoledo08@gmail.com>
2026-06-17 16:56:54 +0800

    Removed Unused

--- common/src/main/java/com/lay/betterbadges/common/config/ModConfigurationException.java
+++
@@ -1,7 +0,0 @@
-package com.lay.betterbadges.common.config;
-
-public class ModConfigurationException extends RuntimeException {
-    public ModConfigurationException(String message) {
-        super(message);
-    }
-}
