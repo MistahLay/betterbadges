2fdef4784b61352c9495487b0ca285c109f548a8
MistahLay <clarencetoledo08@gmail.com>
2026-06-19 18:20:09 +0800

    Elytra attribute logic moved to elytra events

--- fabric/src/main/java/com/lay/betterbadges/fabric/BetterBadgesFabric.java
+++ fabric/src/main/java/com/lay/betterbadges/fabric/BetterBadgesFabric.java
@@ -1,13 +1,21 @@
 package com.lay.betterbadges.fabric;
 
 import com.lay.betterbadges.common.BetterBadges;
+import com.lay.betterbadges.common.api.attribute.vanilla.MiscVanillaAttributes;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
 
 public final class BetterBadgesFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
         BetterBadges.init();
+
+        this.listenEvents();
+    }
+
+    public void listenEvents(){
+        EntityElytraEvents.CUSTOM.register((entity, tickElytra) -> MiscVanillaAttributes.hasNaturalElytra(entity));
     }
 
 }
